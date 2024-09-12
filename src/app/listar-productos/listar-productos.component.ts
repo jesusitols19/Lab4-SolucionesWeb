53269d8d2fec6517eb3b975d37b893314b663de0
SrEnriqueX <jechuramirez1999@gmail.com>
2024-09-12 00:34:34 -0500

    Tercera version

--- src/app/listar-productos/listar-productos.component.ts
+++ src/app/listar-productos/listar-productos.component.ts
@@ -11,11 +11,11 @@ export class ListarProductosComponent {
 
   productos:Producto[] = [
     {nombre: "LAPTOP-PRO", precio: 4500, descripcion: "1 TB SSD, 16 GB RAM, Intel i7", imagen: "https://alquilerdelaptops.pe/wp-content/uploads/2023/05/descarga-3-1-1024x576.webp",oculto:false},
-    {nombre: "MONITOR-ULTRAWIDE", precio: 1200, descripcion: "34 pulgadas, resolución 4K", imagen: "https://www.smartbusiness.pe/cdn/shop/products/MONITOR-LED-29-LG-29WQ600-W-ULTRAWIDE-2560X1080-HDMI-DP-USB-C-1MS75HZFREESYNC.jpg?v=1723674593",oculto:false},
-    {nombre: "TECLADO-MECANICO", precio: 250, descripcion: "Retroiluminado RGB, switches azules", imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCYRsaLauyyvFhkRLLQj8SorsSUl6Bm7Sr5Q&s",oculto:false},
-    {nombre: "MOUSE-GAMER", precio: 150, descripcion: "Sensor óptico 16000 DPI, 8 botones programables", imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-8vvTinNnPUmy-kLGhSTFznuPuBBTOgKNnw&s",oculto:false},
-    {nombre: "AURICULARES-SURROUND", precio: 300, descripcion: "Sonido envolvente 7.1, micrófono con cancelación de ruido", imagen: "https://rimage.ripley.com.pe/home.ripley/Attachment/MKP/4365/PMP20000326219/full_image-1.jpeg",oculto:false},
-    {nombre: "SILLA-GAMER", precio: 800, descripcion: "Ajustable en altura y reclinable, cojines para cuello y espalda", imagen: "https://www.smartbusiness.pe/cdn/shop/files/7163161-silla-gamer-teros-negro-y-rojo.jpg?v=1716591436",oculto:false}
+    {nombre: "MONITOR-ULTRAWIDE", precio: 1200, descripcion: "34 pulgadas, resolución 4K", imagen: "https://images.samsung.com/is/image/samsung/pe-h890wjl-lc34h890wjlxpe-frontblack-thumb-249123675?$480_480_PNG$",oculto:false},
+    {nombre: "TECLADO-MECANICO", precio: 250, descripcion: "Retroiluminado RGB, switches azules", imagen: "https://dojiw2m9tvv09.cloudfront.net/23332/product/X_t-tgk315-46416.png?33&time=1725373989",oculto:false},
+    {nombre: "MOUSE-GAMER", precio: 150, descripcion: "Sensor óptico 16000 DPI, 8 botones programables", imagen: "https://i0.wp.com/nasabytechzone.com/wp-content/uploads/2023/05/NS_GM05-1.png?fit=1000%2C1000&ssl=1",oculto:false},
+    {nombre: "AURICULARES-SURROUND", precio: 300, descripcion: "Sonido envolvente 7.1, micrófono con cancelación de ruido", imagen: "https://www.datacont.com/web/image/product.template/2716/image_1024?unique=0360674",oculto:false},
+    {nombre: "SILLA-GAMER", precio: 800, descripcion: "Ajustable en altura y reclinable, cojines para cuello y espalda", imagen: "https://dojiw2m9tvv09.cloudfront.net/48881/product/078844.png",oculto:false}
 
   ];
 
